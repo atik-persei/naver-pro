1b16e8c635309f948decf686a5c3d0ebb41386f5
atik-persei <atikpersei@gmail.com>
2024-03-02 10:50:51 +0000

    ✨ feat: Add branching for tocElement behavior

--- src/pages/content/toc/app.tsx
+++ src/pages/content/toc/app.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from 'react';
+import ConfigManager from '@root/src/shared/storage/ConfigManager';
 
 export default function App() {
-  // toc 분류 데이터
+  // 설정 데이터 구성
+  const [configManager, setConfigManager] = useState(new ConfigManager());
+
+  /*
+  configManager.updateConfigManager({ GlobalApplicationScope: false })
+  console.log(configManager.getConfigManager())
+  */
+
+  // toc 데이터 구성
   const [criterionObject, setCriterionObject] = useState(null);
 
   // 페이지 이동 시 조회
@@ -9,82 +18,162 @@ export default function App() {
     document.querySelector('#mainFrame').addEventListener('load', fetchCriterionObject);
   }, []);
 
-  // toc 분류 데이터 업데이트 시 동작
   useEffect(() => {
-    // toc 분류 데이터 존재 유무 확인
-    if (criterionObject == null) { return; }
+    // 초기 로딩
+    if (criterionObject == null) {
+      return
+    }
 
     // iframe 블로그 본문 조회
     const iframeElement = document.querySelector('#mainFrame') as HTMLIFrameElement
     const iframeContent = iframeElement.contentDocument || iframeElement.contentWindow.document
 
     // toc 제목 조회
-    const titleElements = iframeContent.querySelectorAll(criterionObject.toc)
+    console.log(criterionObject)
+    const titleElements = criterionObject ? iframeContent.querySelectorAll(criterionObject.toc) : []
+
+    // 선택된 blockquote 엘리먼트에 대한 작업 수행
+    const filteredTitleElement = Array.from(titleElements).filter(element => {
+
+      // 정규 표현식을 사용하여 단어 사이의 공백만 제거하고 길이를 계산
+      const textWithoutExtraSpaces = element.textContent.replace(' ', '').trim();
+      return textWithoutExtraSpaces.length <= 200;
+    });
 
     // toc 업데이트
-    tocElementController('update', titleElements)
+    tocElementUpdate(iframeContent, filteredTitleElement)
   }, [criterionObject])
 
-  function tocElementController(type, titleElements?) {
-    const mainFrame = document.querySelector('#mainFrame');
-
-    switch (type) {
-      case 'init':
-        if (mainFrame instanceof HTMLIFrameElement) {
-          // iframe 내부의 document 조회
-          const iframeDocument = mainFrame.contentDocument || mainFrame.contentWindow.document;
-
-          // link 요소 생성 및 확장프로그램의 로컬 CSS 파일 추가
-          var linkElement = iframeDocument.createElement('link');
-          linkElement.rel = 'stylesheet';
-          linkElement.type = 'text/css';
-          linkElement.href = chrome.runtime.getURL('/assets/css/tocStyle.css');
-
-          // head에 link 요소 추가
-          iframeDocument.head.insertBefore(linkElement, iframeDocument.head.children[0]);
-
-          // 게시글 요소 조회
-          const documentBodyElement = iframeDocument.querySelector('.se-viewer');
-
-          // 자식 요소 생성 및 추가
-          const createTocElement = document.createElement('div');
-          createTocElement.classList.add('tocElement', 'se-component-content');
-
-          // 제목과 본문 사이에 요소 추가
-          documentBodyElement.insertBefore(createTocElement, documentBodyElement.children[1]);
+  async function fetchCriterionObject() {
+    setCriterionObject('loading')
+    return new Promise((resolve, reject) => {
+      chrome.runtime.sendMessage({ action: 'getCriterionObject' }, function (response) {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+          return;
         }
-        break;
-      case 'update':
-        if (mainFrame instanceof HTMLIFrameElement) {
-          // iframe 내부의 document 조회
-          const iframeDocument = mainFrame.contentDocument || mainFrame.contentWindow.document;
 
-          // toc 요소 조회
-          const tocElement = iframeDocument.querySelector('.tocElement');
-
-          // 인자 검사
-          if (!titleElements) { return }
-
-          // 목차 요소 추가
-          if ([...titleElements].length == 0) {
-            // 검색된 목차가 없을 경우
-            const spanElement = document.createElement('p')
-            spanElement.classList.add('tocTitle')
-            spanElement.innerText = '목차가 없습니다.'
-            tocElement.appendChild(spanElement)
-          } else {
-            // 검색된 목차가 있을 때
-            [...titleElements].map((titleElement) => {
-              const spanElement = document.createElement('span')
-              spanElement.classList.add('tocTitle')
-              spanElement.innerText = '• ' + titleElement.querySelector('.se-quote').innerText
-              spanElement.addEventListener('click', () => clickHandler(titleElement));
-
-              tocElement.appendChild(spanElement)
-            })
-          }
+        // 데이터 조회 성공 여부 확인
+        const criterionObjectData = response.data;
+        if (!criterionObjectData || !criterionObjectData.result) {
+          reject('Invalid criterion object data');
+          return;
         }
-        break;
+
+        // 필요 데이터 추출
+        const urlPath = window.location.pathname.split('/')[1];
+        const isSpecifiedBlog = criterionObjectData.result.find(criterionObject => criterionObject.url === urlPath) ? true : false;
+
+        // 해당 블로그 설정이 있다면 데이터 반환
+        if (isSpecifiedBlog) {
+          setCriterionObject(criterionObjectData.result.find(criterionObject => criterionObject.url === urlPath))
+          return
+        }
+
+        // 해당 블로그 설정이 없고, 전역 디폴트가 있다면 데이터 반환
+        if (!isSpecifiedBlog && configManager.getConfigManager().GlobalApplicationScope) {
+          setCriterionObject(criterionObjectData.default)
+          return
+        }
+
+        // 블로그 설정, 전역 디폴트가 없다면 데이터 반환
+        setCriterionObject('none')
+        return
+      });
+    });
+  }
+
+  function tocElementInit(iframeContent) {
+    // link 요소 생성 및 확장프로그램의 로컬 CSS 파일 추가
+    const linkElement = iframeContent.createElement('link');
+    linkElement.rel = 'stylesheet';
+    linkElement.type = 'text/css';
+    linkElement.href = chrome.runtime.getURL('/assets/css/tocStyle.css');
+
+    // head에 link 요소 추가
+    iframeContent.head.insertBefore(linkElement, iframeContent.head.children[0]);
+
+    // 게시글 요소 조회
+    const documentBodyElement = iframeContent.querySelector('.se-viewer');
+
+    // 자식 요소 생성 및 추가
+    const createTocElement = document.createElement('div');
+    createTocElement.classList.add('tocElement', 'se-component-content');
+
+    // 제목과 본문 사이에 요소 추가
+    documentBodyElement.insertBefore(createTocElement, documentBodyElement.children[1]);
+  }
+
+  function tocElementUpdate(iframeContent, titleElements) {
+    let tocElement = iframeContent.querySelector('.tocElement');
+
+    // 내용 초기화
+    if (tocElement) {
+      // 모든 자식 요소 삭제
+      while (tocElement.firstChild) {
+        tocElement.removeChild(tocElement.firstChild);
+      }
+    }
+
+    if (!tocElement) {
+      // link 요소 생성 및 확장프로그램의 로컬 CSS 파일 추가
+      const linkElement = iframeContent.createElement('link');
+      linkElement.rel = 'stylesheet';
+      linkElement.type = 'text/css';
+      linkElement.href = chrome.runtime.getURL('/assets/css/tocStyle.css');
+
+      // head에 link 요소 추가
+      iframeContent.head.insertBefore(linkElement, iframeContent.head.children[0]);
+
+      // 게시글 요소 조회
+      const documentBodyElement = iframeContent.querySelector('.se-viewer');
+
+      // 자식 요소 생성 및 추가
+      const createTocElement = document.createElement('div');
+      createTocElement.classList.add('tocElement', 'se-component-content');
+
+      // 제목과 본문 사이에 요소 추가
+      documentBodyElement.insertBefore(createTocElement, documentBodyElement.children[1]);
+
+      tocElement = iframeContent.querySelector('.tocElement');
+    }
+
+    if (criterionObject == 'loading') {
+      const spanElement = document.createElement('p');
+      spanElement.classList.add('tocTitle');
+      spanElement.innerText = '목차를 불러오는 중입니다.';
+      tocElement.appendChild(spanElement);
+      return
+    }
+
+    if (criterionObject == 'none') {
+      const spanElement = document.createElement('p');
+      spanElement.classList.add('tocTitle');
+      spanElement.innerText = '지정된 블로그가 아닙니다.';
+      tocElement.appendChild(spanElement);
+      return
+    }
+
+
+    if (titleElements.length > 0) {
+      titleElements.map((titleElement) => {
+        const spanElement = document.createElement('span');
+        spanElement.classList.add('tocTitle');
+        spanElement.innerText = '• ' + titleElement.querySelectorAll('p')[0].textContent;
+        spanElement.addEventListener('click', () => clickHandler(titleElement));
+
+        tocElement.appendChild(spanElement);
+      })
+
+      return
+    }
+
+    if (titleElements.length == 0) {
+      const spanElement = document.createElement('p');
+      spanElement.classList.add('tocTitle');
+      spanElement.innerText = '목차가 없습니다.';
+      tocElement.appendChild(spanElement);
+      return
     }
   }
 
@@ -102,36 +191,5 @@ export default function App() {
     });
   }
 
-
-  async function fetchCriterionObject() {
-    // TOC 인터페이스 생성
-    tocElementController('init')
-
-    return new Promise((resolve, reject) => {
-      chrome.runtime.sendMessage({ action: 'getCriterionObject' }, function (response) {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
-          return;
-        }
-
-        const criterionObjectData = response.data;
-        if (!criterionObjectData || !criterionObjectData.result) {
-          reject("Invalid criterion object data");
-          return;
-        }
-
-        const urlPath = window.location.pathname.split('/')[1];
-        const urlMatched = criterionObjectData.result.find(criterionObject => criterionObject.url === urlPath);
-        if (!urlMatched) {
-          reject("URL not matched in criterion object data");
-          return;
-        }
-
-        const criterionObject = urlMatched;
-        setCriterionObject(criterionObject)
-      });
-    });
-  }
-
-  return null;
+  return null
 }
