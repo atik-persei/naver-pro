cdfdf09f87b2652b5d8b22f740097a329ba9de32
atik-persei <atikpersei@gmail.com>
2024-03-04 13:46:22 +0000

    ✨ feat: Add extension settings feature

--- src/shared/storage/configStorage.ts
+++ src/shared/storage/configStorage.ts
@@ -1,25 +1,39 @@
 import { BaseStorage, createStorage, StorageType } from '@src/shared/storage/base';
 
 type ConfigStorage = BaseStorage<Config> & {
-  toggle: () => Promise<void>;
+  toggle: (type: string) => Promise<void>;
 };
 
 type Config = {
-  GlobalApplicationScope: boolean;
+  Toc: boolean
+  TocGlobalApplicationScope: boolean;
 };
 
-const storage = createStorage<Config>('config', { GlobalApplicationScope: false }, {
+const storage = createStorage<Config>('config', { Toc: true, TocGlobalApplicationScope: false }, {
   storageType: StorageType.Local,
   liveUpdate: true,
 });
 
 const configStorage: ConfigStorage = {
   ...storage,
-  toggle: async () => {
-    await storage.set((currentConfig) => ({
-      ...currentConfig,
-      GlobalApplicationScope: !currentConfig.GlobalApplicationScope,
-    }));
+  toggle: async (type) => {
+    if (type == 'toc') {
+      await storage.set((currentConfig) => ({
+        ...currentConfig,
+        Toc: !currentConfig.Toc,
+      }));
+
+      await storage.set((currentConfig) => ({
+        ...currentConfig,
+        TocGlobalApplicationScope: false,
+      }));
+    }
+    if (type == 'toc-global-application-scope') {
+      await storage.set((currentConfig) => ({
+        ...currentConfig,
+        TocGlobalApplicationScope: !currentConfig.TocGlobalApplicationScope,
+      }));
+    }
   },
 };
 
