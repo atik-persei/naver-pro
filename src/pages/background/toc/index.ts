f3ecd3512274080ab5468b8978dfa418232afdb4
atik-persei <atikpersei@gmail.com>
2024-03-07 13:29:19 +0000

    ✨ chore: Update CriterionObject query endpoint

--- src/pages/background/toc/index.ts
+++ src/pages/background/toc/index.ts
@@ -2,7 +2,6 @@ import reloadOnUpdate from 'virtual:reload-on-update-in-background-script';
 import 'webextension-polyfill';
 
 reloadOnUpdate('pages/background');
-
 reloadOnUpdate('pages/content/style.scss');
 
 chrome.runtime.onMessage.addListener(
@@ -10,9 +9,13 @@ chrome.runtime.onMessage.addListener(
     if (request.action === 'getCriterionObject') {
       // fetchData 함수를 Promise로 감싸기
       async function fetchCriterionObject() {
-        const response = await fetch(`https://rbxgrbhozxbbhloyrv54gcgkci0xmagq.lambda-url.ap-northeast-2.on.aws`);
-        const result = await response.json();
-        return result;
+        try {
+          const response = await fetch(`https://aiaczbgnw7e3ek2jccxg23moo40mltvm.lambda-url.ap-northeast-2.on.aws`);
+          const result = await response.json();
+          return result;
+        } catch {
+          return 'update'
+        }
       }
 
       // fetchData 호출 후 sendResponse 호출
